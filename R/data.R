93312b8f1b3d197fcaf2b747c7d63c64818fa8ac
Arash <45104106+arashHaratian@users.noreply.github.com>
2022-09-05 20:49:47 +0200

    fixing doc error

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #' @format A data frame with 18 rows and 3 columns:
 #' \describe{
 #'   \item{v1}{names of the nodes, there is an edge between node in `v1` and `v2`, numeric value with no decimals}
-#'   \item{v1}{names of the nodes, there is an edge between node in `v1` and `v2`, numeric value with no decimals}
+#'   \item{v2}{names of the nodes, there is an edge between node in `v1` and `v2`, numeric value with no decimals}
 #'   \item{w}{The weight of the edges, numeric value}
 #' }
 #' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
