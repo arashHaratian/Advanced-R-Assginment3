d71f67116b92ff8cc000ee49a6f4a01a0195a156
ddiazron <112702667+ddiazron@users.noreply.github.com>
2022-09-05 11:58:08 +0200

    Adding euclidean.R to R/

---
+++ R/euclidean.R
@@ -0,0 +1,23 @@
+euclidean <- function(x,y){
+  # We first check whether the arguments are integer
+  stopifnot(is.vector(x=x,mode="numeric"), length(x)==1, x %% 1 == 0, 
+            is.vector(x=y,mode="numeric"), length(y)==1, y %% 1 == 0)
+  
+  # We sort the vector to get the smallest number
+  ordered_vector = sort(c(x, y), decreasing = FALSE)
+  
+  if ((ordered_vector[2] %% ordered_vector[1]) == 0) {
+    return(ordered_vector[1])
+  } else{
+    for (i in 2:ordered_vector[1]) {
+      # We check if the divisors of the smallest number are also divisor of the greatest,
+      # starting with the greatest divisor
+      if ((ordered_vector[1] %% i) == 0) {
+        if ((ordered_vector[2] %% (ordered_vector[1] / i)) == 0) {
+          return(ordered_vector[1] / i)
+        }
+      }
+    }
+    return("There are no common divisors")
+  }
+}
